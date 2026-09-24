cd8cf11c78faa3c0f0b017da706b00d923092878
PST <pstmac@PSTs-MacBook-Pro-2.local>
2026-09-24 07:44:24 +0530

    23 sep 206

---
+++ test14.py
@@ -0,0 +1,52 @@
+# functions 
+# function with and without default arguments 
+# def greet():
+#     return "good morning"
+# res = greet()
+# print(res)
+
+# def greet(name):
+#     return f"good morning {name}"
+
+# # res = greet()
+# # print(res) # TypeError: greet() missing 1 required positional argument
+# name = input("Enter your name: ")
+# res = greet(name)
+# print(res) # good morning Saurabh
+
+# def greet(name = "user"):
+#     return f"good morning {name}"
+
+# res = greet()
+# print(res) # good morning user
+
+# res = greet("Sujata")
+# print(res) # good morning Sujata
+
+# def greet(name = "user" , timing = "evening"):
+#     return f"good {timing} {name}"
+
+# res = greet()
+# print(res) # good evening user
+
+# res = greet(name="sujata")
+# print(res) # good evening sujata
+
+# name = "sujata" "afternoon"
+# print(type(name)) # <class 'tuple'>
+# res = greet(name)
+# print(res) # good evening ('sujata', 'afternoon')
+
+# def greet(name = "user" , timing = "evening"):
+#     return f"good {timing} {name}"
+
+# res = greet(timing="afternoon" , name="Saurabh")
+# print(res) # good afternoon Saurabh
+
+# why functions are there 
+# write once use again 
+# this is what function do 
+# you can call it anywhere once declared 
+
+# lost in space -> will robinson
+#  
